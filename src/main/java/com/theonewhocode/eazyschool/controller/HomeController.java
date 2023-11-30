779c514646fb38de4bfad305749ff52265e9862c
ycssona <ycssona@gmail.com>
2023-11-30 16:25:03 +0530

    Added thymeleaf support

--- src/main/java/com/theonewhocode/eazyschool/controller/HomeController.java
+++ src/main/java/com/theonewhocode/eazyschool/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.theonewhocode.eazyschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = {"", "/", "home"})
-    public String displayHomePage() {
+    public String displayHomePage(Model model) {
+        // Model interface: container between UI and backend
+        model.addAttribute("username", "Jane Doe");
         return "home.html";
     }
 }
