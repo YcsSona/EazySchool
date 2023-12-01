79a2877bea4bc1d72d40fadad0f5211cb522fc0f
ycssona <ycssona@gmail.com>
2023-12-01 20:10:08 +0530

    Building Contact Page

---
+++ src/main/java/com/theonewhocode/eazyschool/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.theonewhocode.eazyschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+
+    @RequestMapping("/contact")
+    public String displayContactPage(){
+        return "contact.html";
+    }
+}
