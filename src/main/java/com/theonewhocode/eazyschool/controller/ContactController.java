c9e57e01714ba4d3df92f70519fa2c2e6cc290d8
ycssona <ycssona@gmail.com>
2023-12-01 23:02:21 +0530

    Submit info from contact form using @RequestParam

--- src/main/java/com/theonewhocode/eazyschool/controller/ContactController.java
+++ src/main/java/com/theonewhocode/eazyschool/controller/ContactController.java
@@ -1,13 +1,31 @@
 package com.theonewhocode.eazyschool.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ContactController {
 
+    private final Logger log = LoggerFactory.getLogger(ContactController.class);
+
     @RequestMapping("/contact")
-    public String displayContactPage(){
+    public String displayContactPage() {
         return "contact.html";
+    }
+
+    @RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
+    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum, @RequestParam String email,
+                                    @RequestParam String subject, @RequestParam String message) {
+        log.info("Name : " + name);
+        log.info("Mobile Number : " + mobileNum);
+        log.info("Email : " + email);
+        log.info("Subject : " + subject);
+        log.info("Message : " + message);
+        return new ModelAndView("redirect:/contact");
     }
 }
