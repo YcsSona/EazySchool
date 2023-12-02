7f57f8c2672cbc6cbd5e059b2fd7033e9defdd3e
ycssona <ycssona@gmail.com>
2023-12-02 11:47:03 +0530

    Added support of lombok

--- src/main/java/com/theonewhocode/eazyschool/model/Contact.java
+++ src/main/java/com/theonewhocode/eazyschool/model/Contact.java
@@ -1,5 +1,8 @@
 package com.theonewhocode.eazyschool.model;
 
+import lombok.Data;
+
+@Data
 public class Contact {
 
     private String name;
@@ -8,54 +11,4 @@ public class Contact {
     private String subject;
     private String message;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMobileNum() {
-        return mobileNum;
-    }
-
-    public void setMobileNum(String mobileNum) {
-        this.mobileNum = mobileNum;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "name='" + name + '\'' +
-                ", mobileNum='" + mobileNum + '\'' +
-                ", email='" + email + '\'' +
-                ", subject='" + subject + '\'' +
-                ", message='" + message + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/theonewhocode/eazyschool/model/Holiday.java
+++ src/main/java/com/theonewhocode/eazyschool/model/Holiday.java
@@ -1,28 +1,13 @@
 package com.theonewhocode.eazyschool.model;
 
+import lombok.Data;
+
+@Data
 public class Holiday {
 
     private final String day;
     private final String reason;
     private final Type type;
-
-    public Holiday(String day, String reason, Type type) {
-        this.day = day;
-        this.reason = reason;
-        this.type = type;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public Type getType() {
-        return type;
-    }
 
     public enum Type {
         FESTIVAL, FEDERAL
