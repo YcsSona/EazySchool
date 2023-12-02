b1984d786ffd7cd8c47380046bb3e1d709b5ef4a
ycssona <ycssona@gmail.com>
2023-12-02 11:53:02 +0530

    Replace boilerplate code with @Slf4j annotation for logging

--- src/main/java/com/theonewhocode/eazyschool/controller/ContactController.java
+++ src/main/java/com/theonewhocode/eazyschool/controller/ContactController.java
@@ -2,6 +2,7 @@ package com.theonewhocode.eazyschool.controller;
 
 import com.theonewhocode.eazyschool.model.Contact;
 import com.theonewhocode.eazyschool.service.ContactService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,10 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class ContactController {
-
-    private final Logger log = LoggerFactory.getLogger(ContactController.class);
 
     private final ContactService contactService;
 
--- src/main/java/com/theonewhocode/eazyschool/service/ContactService.java
+++ src/main/java/com/theonewhocode/eazyschool/service/ContactService.java
@@ -1,14 +1,14 @@
 package com.theonewhocode.eazyschool.service;
 
 import com.theonewhocode.eazyschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ContactService {
-
-    private final Logger log = LoggerFactory.getLogger(ContactService.class);
 
     public boolean saveMessageDetails(Contact contact) {
         boolean isSaved = true;
