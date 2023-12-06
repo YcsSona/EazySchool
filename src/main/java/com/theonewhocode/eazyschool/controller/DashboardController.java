ced1afa440f6853fa11d797b45b80f9372a38e96
ycssona <ycssona@gmail.com>
2023-12-06 21:18:36 +0530

    Added support of global exception handling

--- src/main/java/com/theonewhocode/eazyschool/controller/DashboardController.java
+++ src/main/java/com/theonewhocode/eazyschool/controller/DashboardController.java
@@ -12,6 +12,7 @@ public class DashboardController {
     public String displayDashboard(Model model, Authentication authentication) {
         model.addAttribute("username", authentication.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
+//        throw new RuntimeException("It's been a bad day!!");
         return "dashboard.html";
     }
 }
