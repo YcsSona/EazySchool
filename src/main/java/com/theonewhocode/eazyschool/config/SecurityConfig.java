797a0a29f9276d54e74811f12b3a91e8efbbaef5
ycssona <ycssona@gmail.com>
2023-12-18 21:26:20 +0530

    Implement Pagination & Dynamic Sorting

--- src/main/java/com/theonewhocode/eazyschool/config/SecurityConfig.java
+++ src/main/java/com/theonewhocode/eazyschool/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
                         .ignoringRequestMatchers(mvcMatcherBuilder.pattern("/public/**")))
                 .authorizeHttpRequests((requests) -> requests.requestMatchers(mvcMatcherBuilder.pattern("/dashboard")).authenticated()
                         .requestMatchers(mvcMatcherBuilder.pattern("/admin/**")).hasRole("ADMIN")
-                        .requestMatchers(mvcMatcherBuilder.pattern("/displayMessages")).hasRole("ADMIN")
+                        .requestMatchers(mvcMatcherBuilder.pattern("/displayMessages/**")).hasRole("ADMIN")
                         .requestMatchers(mvcMatcherBuilder.pattern("/closeMsg/**")).hasRole("ADMIN")
                         .requestMatchers(mvcMatcherBuilder.pattern("/displayProfile")).authenticated()
                         .requestMatchers(mvcMatcherBuilder.pattern("/updateProfile")).authenticated()
--- src/main/java/com/theonewhocode/eazyschool/controller/ContactController.java
+++ src/main/java/com/theonewhocode/eazyschool/controller/ContactController.java
@@ -4,13 +4,11 @@ import com.theonewhocode.eazyschool.model.Contact;
 import com.theonewhocode.eazyschool.service.ContactService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -54,18 +52,28 @@ public class ContactController {
         return "redirect:/contact"; // new request to html page
     }
 
-    @RequestMapping("/displayMessages")
-    public ModelAndView displayMessages(Model model) {
-        List<Contact> contactMsgs = contactService.findMsgsWithOpenStatus();
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("messages.html");
-        modelAndView.addObject("contactMsgs", contactMsgs);
+    @RequestMapping("/displayMessages/page/{pageNum}")
+    public ModelAndView displayMessages(Model model,
+                                        @PathVariable int pageNum,
+                                        @RequestParam String sortField,
+                                        @RequestParam String sortDir) {
+        Page<Contact> msgPage = contactService.findMsgsWithOpenStatus(pageNum, sortField, sortDir);
+        // fetches the records associated with current page
+        List<Contact> contactMsgs = msgPage.getContent();
+        ModelAndView modelAndView = new ModelAndView("messages.html");
+        model.addAttribute("currentPage", pageNum);
+        model.addAttribute("totalPages", msgPage.getTotalPages());
+        model.addAttribute("totalMsgs", msgPage.getTotalElements());
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+        modelAndView.addObject("contactMsgs",contactMsgs);
         return modelAndView;
     }
 
     @RequestMapping("/closeMsg")
     public String closeMsg(@RequestParam int id) {
         contactService.updateMsgStatus(id);
-        return "redirect:/displayMessages";
+        return "redirect:/displayMessages/page/1?sortField=name&sortDir=desc";
     }
 }
