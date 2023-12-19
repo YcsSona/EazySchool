8c14da2a72ed84fbd37c94d431a96c5793309744
ycssona <ycssona@gmail.com>
2023-12-19 16:40:37 +0530

    Accepting data from request body & @RequestBody

--- src/main/java/com/theonewhocode/eazyschool/rest/ContactRestController.java
+++ src/main/java/com/theonewhocode/eazyschool/rest/ContactRestController.java
@@ -5,10 +5,7 @@ import com.theonewhocode.eazyschool.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,14 @@ public class ContactRestController {
     @ResponseBody
     public List<Contact> getMessagesByStatus(@RequestParam String status) {
         return contactRepository.findByStatus(status);
+    }
+
+    @GetMapping("/getAllMsgsByStatus")
+    @ResponseBody
+    public List<Contact> getAllMsgsByStatus(@RequestBody Contact contact) {
+        if (contact != null && contact.getStatus() != null) {
+            return contactRepository.findByStatus(contact.getStatus());
+        }
+        return List.of();
     }
 }
