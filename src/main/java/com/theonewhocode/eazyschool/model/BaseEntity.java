d25f58285fc8d2042efb6f407508d6823d56863f
ycssona <ycssona@gmail.com>
2023-12-19 20:12:12 +0530

    Content filter inside Rest Services using @JsonIgnore annotation

--- src/main/java/com/theonewhocode/eazyschool/model/BaseEntity.java
+++ src/main/java/com/theonewhocode/eazyschool/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.theonewhocode.eazyschool.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -19,17 +20,21 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false) // Do not consider updating of values when update operation is being done on DB
+    @JsonIgnore
     private LocalDateTime createdAt;
 
     @CreatedBy
     @Column(updatable = false)
+    @JsonIgnore
     private String createdBy;
 
     @LastModifiedDate
     @Column(insertable = false) // Do not consider for inserting of values when insert operation being done on DB
+    @JsonIgnore
     private LocalDateTime updatedAt;
 
     @LastModifiedBy
     @Column(insertable = false)
+    @JsonIgnore
     private String updatedBy;
 }
